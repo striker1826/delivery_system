ea99304145fd9208c47cd08b8f62243842fe31b0
김민섭 <alstjq123579@daum.net>
2023-06-28 09:09:45 +0900

    feat: login 테스트 코드 및 대응 코드 작성 중

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,12 +1,13 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
+import { FakeAuthRepository } from '../../../test/auth/auth.service.spec';
 import * as bcrypt from 'bcrypt';
-import { FakeAuthRepository } from 'test/auth/auth.service.spec';
+
 @Injectable()
 export class AuthService {
   authRepository = new FakeAuthRepository();
 
   async signUp(id: string, password: string) {
-    const user = await this.authRepository.findUserById(id);
+    const user = await this.authRepository.findUserByUserId(id);
     if (user) {
       throw new BadRequestException('이미 존재하는 아이디 입니다');
     }
@@ -14,5 +15,21 @@ export class AuthService {
     const hashedPassword = await bcrypt.hash(password, 10);
     await this.authRepository.createUser(id, hashedPassword);
     return;
+  }
+
+  async login(id: string, password: string) {
+    const user = await this.authRepository.findUserByUserId(id);
+    if (!user) {
+      throw new BadRequestException(
+        '아이디 혹은 비밀번호를 다시 확인해 주세요',
+      );
+    }
+
+    const checkPassword = await bcrypt.compare(password, user.password);
+    if (!checkPassword) {
+      throw new BadRequestException(
+        '아이디 혹은 비밀번호를 다시 확인해 주세요',
+      );
+    }
   }
 }
--- test/auth/auth.service.spec.ts
+++ test/auth/auth.service.spec.ts
@@ -1,14 +1,16 @@
 import { BadRequestException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from '../../src/modules/auth/auth.service';
-
+import * as bcrypt from 'bcrypt';
 export class FakeAuthRepository {
-  async findUserById(id: string) {
+  async findUserByUserId(id: string) {
+    const password = '1234';
+    const hashedPassword = await bcrypt.hash(password, 10);
     if (id === 'test1') {
       return {
         id: 1,
         userId: 'test1',
-        password: '1234',
+        password: hashedPassword,
       };
     }
     return;
@@ -48,10 +50,37 @@ describe('AuthService', () => {
     });
 
     it('signUp()의 리턴값이 null인지 확인', async () => {
-      const id = 'test2';
+      const id = 'test999';
       const password = 'asdf';
       const result = await authService.signUp(id, password);
       expect(result).toBeNull;
+    });
+  });
+
+  describe('login', () => {
+    it('입력받은 아이디가 존재하지 않는 경우 - 실패', async () => {
+      const id = 'test999';
+      const password = 'asdf';
+
+      await expect(authService.login(id, password)).rejects.toThrowError(
+        new BadRequestException('아이디 혹은 비밀번호를 다시 확인해 주세요'),
+      );
+    });
+
+    it('입력받은 비밀번호가 아이디의 비밀번호와 일치하지 않는 경우 - 실패', async () => {
+      const id = 'test1';
+      const password = '1111';
+
+      await expect(authService.login(id, password)).rejects.toThrowError(
+        new BadRequestException('아이디 혹은 비밀번호를 다시 확인해 주세요'),
+      );
+    });
+
+    it('로그인이 성공적으로 되었을 경우 Jwt를 리턴한다 - 성공', async () => {
+      const id = 'test1';
+      const password = '1234';
+
+      const result = await authService.login(id, password);
     });
   });
 });
