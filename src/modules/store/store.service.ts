7436925e50e3e01235f26d0d27d07e9d1ef2e561
김민섭 <alstjq123579@daum.net>
2023-06-30 18:27:08 +0900

    feat: createStore() 테스트 코드 및 서비스 코드 작성

--- src/modules/store/store.service.ts
+++ src/modules/store/store.service.ts
@@ -1,4 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from "@nestjs/common";
+import { FakeOwnerRepository, FakeStoreRepository } from "../../../test/store/store.service.spec";
 
 @Injectable()
-export class StoreService {}
+export class StoreService {
+    storeRepository = new FakeStoreRepository();
+    ownerRepository = new FakeOwnerRepository();
+
+    async createStore(OwnerId: number, storeInfo): Promise<void> {
+        const owner = await this.ownerRepository.findOwnerById(OwnerId);
+        if (!owner) {
+            throw new BadRequestException("존재하지 않는 사장님 입니다");
+        }
+        await this.storeRepository.createStore(OwnerId, storeInfo);
+        return;
+    }
+}
