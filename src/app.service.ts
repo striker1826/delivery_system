9f9ef26dd62b0c21f1cc788a5c595aaef6aa093f
김민섭 <alstjq123579@daum.net>
2023-07-03 21:27:55 +0900

    ci: jenkins docker-compose Test

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from "@nestjs/common";
 @Injectable()
 export class AppService {
     getHello(): string {
-        return "Hello Jenkins World!!";
+        return "Hello Jenkins with Docker-compose!";
     }
 }
