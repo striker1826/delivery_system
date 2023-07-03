523bd557db895f57429154dbb8f267c8062d3d3c
김민섭 <alstjq123579@daum.net>
2023-07-03 16:38:00 +0900

    ci: jenkins webhook Test

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from "@nestjs/common";
 @Injectable()
 export class AppService {
     getHello(): string {
-        return "Hello Jenkins with docker-Compose!";
+        return "Hello Jenkins with Compose!";
     }
 }
