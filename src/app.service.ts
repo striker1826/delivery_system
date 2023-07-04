80415e1bc62d5bce141cdbf6e7a8cb926805bd32
김민섭 <alstjq123579@daum.net>
2023-07-04 10:23:58 +0900

    ci: Jenkins --> docker로 변경

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from "@nestjs/common";
 @Injectable()
 export class AppService {
     getHello(): string {
-        return "Hello World Jenkins Dockercompose!";
+        return "Hello World githubActions!";
     }
 }
